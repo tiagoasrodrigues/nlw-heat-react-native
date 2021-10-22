c9134758132d6f023005e6297d807f1e87f91c60
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-10-22 12:55:22 -0300

    :wrench: chore: finishing Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import { Header } from "../../components/Header";
 
 import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>
-        DoWhile 2021
-      </Text>
+      <Header />
     </View>
   )
 }
