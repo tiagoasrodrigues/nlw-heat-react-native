5686d41f8860c79d8682fbaaf19d0009e9510854
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-10-22 09:54:24 -0300

    :wrench: chore: create interface Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from 'react-native';
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        DoWhile 2021
+      </Text>
+    </View>
+  )
+}
